c5fbba3297ff2427401fe135e093a26913ac5208
Scott Vanderlind <scottv@ifixit.com>
2015-07-11 16:42:54 -0700

    Remove hardcoded devices from template
    Instead, define our x10 modules in a JSON file and dynamically
    populate the template by them.

--- routes/modules.js
+++ routes/modules.js
@@ -1,7 +1,14 @@
+var fs = require('fs');
+
 module.exports = function (express) {
    var router = express.Router();
    router.get('/', function (req, res) {
-      res.render('modules');
+      fs.readFile('./devices.json', 'utf8', function (err, data) {
+         if(err) throw err;
+         devices = JSON.parse(data);
+
+         res.render('modules', {devices: devices});
+      });
    });
    return router;
 }
